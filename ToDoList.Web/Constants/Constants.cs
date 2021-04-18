c2cbd91c4e6eea6fbaf0732bc65c254d67c9f718
Brevellnash <brevellnash@gmail.com>
2021-04-18 14:44:54 +1200

    Error handling clean up and comments

---
+++ ToDoList.Web/Constants/Constants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Web.Constants
+{
+    public static class Constants
+    {
+        public static class Views
+        {
+            public const string Index = "Index";
+
+            public const string Error = "Error";
+
+            public const string Edit = "Edit";
+
+            public const string Create = "Create";
+
+            public const string Delete = "Delete";
+        }
+
+
+        public static class Errors
+        {
+            public const string TaskNotFound = "Task was not found";
+
+            public const string IdCannotBeEmpty = "Id cannot be empty";
+
+            public const string FailedLoadOpen = "Failed to load open tasks, please try again";
+
+            public const string FailedLoadClosed = "Failed to load completed tasks, please try again";
+
+            public const string FailedUpdate = "Failed to update task, please try again";
+
+            public const string FailedDelete = "Failed to delete task, please try again";
+
+            public const string FailedCreate = "Failed to create new task, please try again";
+        }
+    }
+}
--- ToDoList.Web/Controllers/ToDoController.cs
+++ ToDoList.Web/Controllers/ToDoController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoList.Web.Constants;
 using ToDoList.Web.Models;
 using ToDoList.Web.Services;
 
@@ -24,26 +25,33 @@ namespace ToDoList.Controllers
         public async Task<IActionResult> Index()
         {
             var openTasks = await _toDoService.GetAsyncOpenTasks();
-
+            if(openTasks == null)
+            {
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.FailedLoadOpen });
+            }
             return View(new ToDoListViewModel() { Title = "To Do List", Tasks = openTasks });
         }
 
         public async Task<IActionResult> CompletedTasks()
         {
             var closedTasks = await _toDoService.GetAsyncClosedTasks();
-
-            return View("Index", new ToDoListViewModel() { Title = "Completed Tasks", Tasks = closedTasks });
+            if (closedTasks == null)
+            {
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.FailedLoadClosed });
+            }
+            return View(Constants.Views.Index, new ToDoListViewModel() { Title = "Completed Tasks", Tasks = closedTasks });
         }
 
         public async Task<IActionResult> Edit(string id)
         {
             if (string.IsNullOrEmpty(id))
             {
-                return NotFound();
+                return View(Constants.Views.Error, new ErrorViewModel(){Message = Constants.Errors.IdCannotBeEmpty});
             }
             var item = await _toDoService.GetAsyncById(id);
-            if (item == null){
-                return NotFound();
+            if (item == null)
+            {
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.TaskNotFound });
             }
             return View(item);
         }
@@ -53,26 +61,26 @@ namespace ToDoList.Controllers
         {
             if (string.IsNullOrEmpty(id))
             {
-                return NotFound();
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.TaskNotFound });
             }
             var success = await _toDoService.UpdateAsync(todo);
             if (success)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction(Constants.Views.Index);
             }
-            return BadRequest("Could not update task, please try again");
+            return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.FailedUpdate });
         }
 
         public async Task<IActionResult> Delete (string id)
         {
             if (string.IsNullOrEmpty(id))
             {
-                return NotFound();
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.IdCannotBeEmpty });
             }
             var item = await _toDoService.GetAsyncById(id);
             if (item == null)
             {
-                return NotFound();
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.TaskNotFound });
             }
             return View(item);
         }
@@ -82,14 +90,14 @@ namespace ToDoList.Controllers
         {
             if (string.IsNullOrEmpty(todo.Id))
             {
-                return NotFound();
+                return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.IdCannotBeEmpty });
             }
             var success = await _toDoService.DeleteAsync(todo.Id);
             if (success)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction(Constants.Views.Index);
             }
-            return BadRequest("Could not delete the task, please try again");
+            return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.FailedDelete });
         }
 
         public ViewResult Create() => View();
@@ -97,14 +105,12 @@ namespace ToDoList.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(ToDoTask todo)
         {
-            await _toDoService.CreateAsync(todo);
-            return RedirectToAction("Index");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var ret = await _toDoService.CreateAsync(todo);
+            if (ret) 
+            {
+                return RedirectToAction(Constants.Views.Index);
+            }
+            return View(Constants.Views.Error, new ErrorViewModel() { Message = Constants.Errors.FailedCreate });
         }
     }
 }
